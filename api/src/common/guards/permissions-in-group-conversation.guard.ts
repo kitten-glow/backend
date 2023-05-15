76ed49a3fa53c82650bb559ebeed304aa9ef3530
me <catofdev@ya.ru>
2023-05-15 23:48:12 +0300

    PermissionsInGroupConversationGuard

---
+++ api/src/common/guards/permissions-in-group-conversation.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ExecutionContext, Injectable, mixin } from '@nestjs/common';
+import { AuthRequest } from '../types/auth-request.interface';
+import { ConversationsService } from '../../modules/conversations/conversations.service';
+import { Prisma } from '@prisma/client';
+
+export const PermissionsInGroupConversationGuard = (
+    permissions: Prisma.GroupConversationParticipantAdminWhereInput,
+) => {
+    @Injectable()
+    class PermissionsInGroupConversationGuardMixin implements CanActivate {
+        constructor(public readonly conversationsService: ConversationsService) {}
+
+        public async canActivate(context: ExecutionContext) {
+            const request = context.switchToHttp().getRequest<AuthRequest>();
+
+            const user = request.auth.user;
+            const conversationId = +request.query.conversationId;
+
+            if (isNaN(conversationId)) {
+                return false;
+            }
+
+            return this.conversationsService.hasPermissionsInGroupConversation(
+                conversationId,
+                user.id,
+                permissions,
+            );
+        }
+    }
+
+    return mixin(PermissionsInGroupConversationGuardMixin);
+};
